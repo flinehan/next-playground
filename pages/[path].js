6a4e49913db792e8b1e78d04f953d5b44c505302
flinehan <frankie.linehan@gmail.com>
2020-12-03 17:33:15 -0800

    loading async paths and props.

--- pages/[path].js
+++ pages/[path].js
@@ -1,19 +1,26 @@
 import { useRouter } from 'next/router'
+import fetchInstructable from '../services/fetchInstructable'
 
-export async function getStaticProps() {
-  const tweet = await fetchTweetAst('1249937011068129280');
-
-  return { props: { tweet } };
+// This function gets called at build time
+export async function getStaticPaths() {
+  return { paths:[], fallback: true }
 }
 
-export default function Instructable() {
+export async function getStaticProps({params}) {
+  const instructableModel = await fetchInstructable(params.path);
+
+  return { props: { instructableModel } };
+}
+
+export default function Instructable({instructableModel}) {
   const router = useRouter()
   const query = router.query
 
-
   return (
-    <div className={styles.container}>
-      {query}
+    <div>
+      {query.path}
+      document:
+      {instructableModel.id}
     </div>
   )
 }
